61e9397dc45629648741b19630bd5447df896cce
Eduardo Henrique Gimenes Custodio <eduardo.gimenes.custodio@gmail.com>
2023-10-11 13:31:53 -0400

    feat: user repository

--- src/main/java/br/com/eduardogimenes/todolist/user/UserModel.java
+++ src/main/java/br/com/eduardogimenes/todolist/user/UserModel.java
@@ -1,7 +1,11 @@
 package br.com.eduardogimenes.todolist.user;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -15,8 +19,12 @@ public class UserModel {
     @GeneratedValue(generator = "UUID")
     private UUID id;
     
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
